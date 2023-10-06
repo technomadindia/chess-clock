cce2cf8fe3f40199bb5e1e6e23e65ac4efe58f00
sumandeepb <sumandeep.banerjee@gmail.com>
2023-10-07 02:50:40 +0530

    add: count down timer logic

--- chess-clock/chess-clock.ino
+++ chess-clock/chess-clock.ino
@@ -1,42 +1,172 @@
 #include <TM1637Display.h>
 
 // hardware pin connections
-const int W_TIMER_CLK_PIN = 5;
-const int W_TIMER_DIO_PIN = 4;
-const int B_TIMER_CLK_PIN = 6;
-const int B_TIMER_DIO_PIN = 7;
-const int WHITE_MOVE_PIN = 2;
-const int BLACK_MOVE_PIN = 3;
-const int START_PAUSE_PIN = 8;
-const int KNOB1_ENC_A_PIN = 14;
-const int KNOB1_ENC_B_PIN = 15;
-const int KNOB1_SW_PIN = 12;
-const int BUZZER_PIN = 13;
+#define W_TIMER_CLK_PIN 5
+#define W_TIMER_DIO_PIN 4
+#define B_TIMER_CLK_PIN 6
+#define B_TIMER_DIO_PIN 7
+#define WHITE_MOVE_PIN 2
+#define BLACK_MOVE_PIN 3
+#define START_PAUSE_PIN 8
+#define KNOB1_ENC_A_PIN 14
+#define KNOB1_ENC_B_PIN 15
+#define KNOB1_SW_PIN 12
+#define BUZZER_PIN 13
+
+// constants
+enum STATES { BEGIN,
+              READY,
+              W_PLAYING,
+              B_PLAYING,
+              PAUSED,
+              END,
+              PLAY_CONFIG,
+              BONUS_CONFIG };
+const uint8_t TIMER_SEG_CLEAR[] = {0x00, 0x00, 0x00, 0x00};
 
 // data
-
-const uint8_t TIMER_SEG_CLEAR[] = {0x00, 0x00, 0x00, 0x00};
-uint8_t w_timer_data[] = {0xff, 0xff, 0xff, 0xff};
-uint8_t b_timer_data[] = {0xff, 0xff, 0xff, 0xff};
+STATES state_machine = STATES::BEGIN;
+long sel_play_time = 600000; // milli seconds
+long sel_bonus_time = 10000;
+long current_time = 0;
+long w_total_time = 0;
+long b_total_time = 0;
+long bonus_time = 0;
+long w_time = 0;
+long b_time = 0;
+uint8_t w_moves = 0;
+uint8_t b_moves = 0;
+long w_diff = 0;
+long b_diff = 0;
+long pause_time = 0;
+long play_time = 0;
 
 // create module object instances
 TM1637Display w_timer_display(W_TIMER_CLK_PIN, W_TIMER_DIO_PIN);
 TM1637Display b_timer_display(B_TIMER_CLK_PIN, B_TIMER_DIO_PIN);
+uint8_t w_timer_data[] = {0xff, 0xff, 0xff, 0xff};
+uint8_t b_timer_data[] = {0xff, 0xff, 0xff, 0xff};
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-  w_timer_display.setBrightness(0x0f);
-  w_timer_display.setSegments(w_timer_data);
-  b_timer_display.setBrightness(0x08);
-  b_timer_display.setSegments(b_timer_data);
+    // initialize LED segment display
+    w_timer_display.setBrightness(0x08);
+    w_timer_display.setSegments(TIMER_SEG_CLEAR);
+    b_timer_display.setBrightness(0x08);
+    b_timer_display.setSegments(TIMER_SEG_CLEAR);
+
+    // initialize I/O pins
+    pinMode(WHITE_MOVE_PIN, INPUT);
+    attachInterrupt(digitalPinToInterrupt(WHITE_MOVE_PIN), white_move, RISING);
+    pinMode(BLACK_MOVE_PIN, INPUT);
+    attachInterrupt(digitalPinToInterrupt(BLACK_MOVE_PIN), black_move, RISING);
+    pinMode(START_PAUSE_PIN, INPUT);
+    pinMode(KNOB1_SW_PIN, INPUT);
+    pinMode(KNOB1_ENC_A_PIN, INPUT);
+    pinMode(KNOB1_ENC_B_PIN, INPUT);
+    pinMode(BUZZER_PIN, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+    switch (state_machine) {
+    default:
+    case STATES::BEGIN:
+        // initialize timers
+        initialize_timers();
+        // update display
+        update_timer_display();
+        // update state
+        state_machine = STATES::READY;
+        break;
+
+    case STATES::READY:
+        // update display
+        update_timer_display();
+        // update state
+        state_machine = STATES::W_PLAYING;
+        break;
+
+    case STATES::W_PLAYING:
+        current_time = millis();
+        w_time = w_total_time - (current_time - w_diff);
+        // update display
+        update_timer_display();
+        break;
+
+    case STATES::B_PLAYING:
+        current_time = millis();
+        b_time = b_total_time - (current_time - b_diff);
+        // update display
+        update_timer_display();
+        break;
+
+    case STATES::PAUSED:
+        break;
+
+    case STATES::END:
+        break;
+    }
+
+    delay(100);
+}
+
+void initialize_timers() {
+    // set time limits
+    w_total_time = sel_play_time;
+    b_total_time = sel_play_time;
+    bonus_time = sel_bonus_time;
+    w_time = w_total_time;
+    b_time = b_total_time;
+    w_moves = 0;
+    b_moves = 0;
+
+    // init timers
+    current_time = millis();
+    w_diff = current_time;
+    b_diff = current_time;
+    play_time = current_time;
+    pause_time = 0;
+}
+
+void convert_time_to_clock(long time, uint8_t clock[]) {
+    long minute = (time / 1000) / 60;
+    long second = (time / 1000) % 60;
+
+    if (minute > 99) { // TODO: fix this temporary hack
+        minute = 99;
+    }
+
+    clock[0] = minute / 10;
+    clock[1] = minute % 10;
+    clock[2] = second / 10;
+    clock[3] = second % 10;
+}
+
+void update_timer_display() {
+    convert_time_to_clock(w_time, w_timer_data);
+    w_timer_data[0] = w_timer_display.encodeDigit(w_timer_data[0]);
+    w_timer_data[1] = w_timer_display.encodeDigit(w_timer_data[1]) | 0x80;
+    // w_timer_data[1] = w_timer_display.encodeDigit(w_timer_data[1]);
+    w_timer_data[2] = w_timer_display.encodeDigit(w_timer_data[2]);
+    w_timer_data[3] = w_timer_display.encodeDigit(w_timer_data[3]);
+
+    convert_time_to_clock(b_time, b_timer_data);
+    b_timer_data[0] = b_timer_display.encodeDigit(b_timer_data[0]);
+    b_timer_data[1] = b_timer_display.encodeDigit(b_timer_data[1]) | 0x80;
+    // b_timer_data[1] = b_timer_display.encodeDigit(b_timer_data[1]);
+    b_timer_data[2] = b_timer_display.encodeDigit(b_timer_data[2]);
+    b_timer_data[3] = b_timer_display.encodeDigit(b_timer_data[3]);
+
+    // refresh LED segment display
+    w_timer_display.setSegments(w_timer_data, 4, 0);
+    b_timer_display.setSegments(b_timer_data, 4, 0);
+    // TM1637RefreshStart = true;
+    // TM1637RefreshFlag = false;
+}
+
+void white_move() {
+}
+
+void black_move() {
 }
