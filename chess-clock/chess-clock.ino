385d7d1268314606891cc29b17ebe18596ae94e5
sumandeepb <sumandeep.banerjee@gmail.com>
2023-10-07 16:45:10 +0530

    add: switch inputs, alternating timers

--- chess-clock/chess-clock.ino
+++ chess-clock/chess-clock.ino
@@ -11,7 +11,7 @@
 #define KNOB1_ENC_A_PIN 14
 #define KNOB1_ENC_B_PIN 15
 #define KNOB1_SW_PIN 12
-#define BUZZER_PIN 13
+#define ALERT_PIN 13
 
 // constants
 enum STATES { BEGIN,
@@ -41,6 +41,12 @@ long b_diff = 0;
 long pause_time = 0;
 long play_time = 0;
 
+// input states
+int start_pause_button_state = LOW;
+int white_move_button_state = LOW;
+int black_move_button_state = LOW;
+int mode_change_button_state = LOW;
+
 // create module object instances
 TM1637Display w_timer_display(W_TIMER_CLK_PIN, W_TIMER_DIO_PIN);
 TM1637Display b_timer_display(B_TIMER_CLK_PIN, B_TIMER_DIO_PIN);
@@ -57,42 +63,64 @@ void setup() {
 
     // initialize I/O pins
     pinMode(WHITE_MOVE_PIN, INPUT);
-    attachInterrupt(digitalPinToInterrupt(WHITE_MOVE_PIN), white_move, RISING);
+    attachInterrupt(digitalPinToInterrupt(WHITE_MOVE_PIN), white_move_button_event, RISING);
     pinMode(BLACK_MOVE_PIN, INPUT);
-    attachInterrupt(digitalPinToInterrupt(BLACK_MOVE_PIN), black_move, RISING);
+    attachInterrupt(digitalPinToInterrupt(BLACK_MOVE_PIN), black_move_button_event, RISING);
     pinMode(START_PAUSE_PIN, INPUT);
     pinMode(KNOB1_SW_PIN, INPUT);
     pinMode(KNOB1_ENC_A_PIN, INPUT);
     pinMode(KNOB1_ENC_B_PIN, INPUT);
-    pinMode(BUZZER_PIN, OUTPUT);
+    pinMode(ALERT_PIN, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
+    start_pause_button_state = digitalRead(START_PAUSE_PIN);
+    mode_change_button_state = digitalRead(KNOB1_SW_PIN);
+
     switch (state_machine) {
     default:
     case STATES::BEGIN:
         initialize_timers();
-        update_w_timer_display();
-        update_b_timer_display();
-        change_state_to (STATES::READY);
+        change_state_to(STATES::READY);
         break;
 
     case STATES::READY:
         update_w_timer_display();
         update_b_timer_display();
-        change_state_to (STATES::B_PLAYING);
+
+        if (HIGH == start_pause_button_state) {
+            change_state_to(STATES::W_PLAYING);
+        }
         break;
 
     case STATES::W_PLAYING:
         current_time = millis();
-        w_time = w_total_time - (current_time - w_diff);
+        if (HIGH == white_move_button_state) {
+            b_diff += current_time - play_time;
+            play_time = current_time;
+            // w_total_time += bonus_time;
+            w_time = w_total_time - (current_time - w_diff);
+            change_state_to(STATES::B_PLAYING);
+            white_move_button_state = LOW;
+        } else {
+            w_time = w_total_time - (current_time - w_diff);
+        }
         update_w_timer_display();
         break;
 
     case STATES::B_PLAYING:
         current_time = millis();
-        b_time = b_total_time - (current_time - b_diff);
+        if (HIGH == black_move_button_state) {
+            w_diff += current_time - play_time;
+            play_time = current_time;
+            // b_total_time += bonus_time;
+            b_time = b_total_time - (current_time - b_diff);
+            change_state_to(STATES::W_PLAYING);
+            black_move_button_state = LOW;
+        } else {
+            b_time = b_total_time - (current_time - b_diff);
+        }
         update_b_timer_display();
         break;
 
@@ -107,7 +135,7 @@ void loop() {
 }
 
 // change STATE MACHINE states
-void change_state_to (STATES final_state) {
+void change_state_to(STATES final_state) {
     state_machine = final_state;
 }
 
@@ -180,9 +208,11 @@ void update_b_timer_display() {
 }
 
 // ISR for white 'move complete' button
-void white_move() {
+void white_move_button_event() {
+    white_move_button_state = HIGH;
 }
 
 // ISR for black 'move complete' button
-void black_move() {
+void black_move_button_event() {
+    black_move_button_state = HIGH;
 }
